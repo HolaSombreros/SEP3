011ef257509dad4f0510c728586c0c649a5aef8a
stefaniatomuta <304173@via.dk>
2021-10-26 15:57:02 +0200

    Merge

--- csharp/SEP3UI/Model/ShoppingCart.cs
+++ csharp/SEP3UI/Model/ShoppingCart.cs
@@ -4,6 +4,10 @@ namespace SEP3UI.Model {
     public class ShoppingCart {
         public User User { get; set; }
         public double Total { get; set; }
-        public IList<Item> Items { get; set; }
+        public IList<Item> Items  { get; set; }
+
+        public ShoppingCart() {
+            Items = new List<Item>();
+        }
     }
 }
