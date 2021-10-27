2fcc2fd86bba6d4e0a3ae6d24ad9a9292b456267
stefaniatomuta <304173@via.dk>
2021-10-27 11:20:30 +0200

    PayoutForm

--- csharp/SEP3UI/Model/Address.cs
+++ csharp/SEP3UI/Model/Address.cs
@@ -2,7 +2,7 @@
     public class Address {
         public string City { get; set; }
         public string Street { get; set; }
-        public int Number { get; set; }
+        public string Number { get; set; }
         public int ZipCode { get; set; }
         
     }
