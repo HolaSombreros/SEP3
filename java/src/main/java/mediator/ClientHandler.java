a966ce3cea9b42c64aabff4033cf1c431603c9c7
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2021-11-02 16:26:35 +0100

    Order details

--- java/src/main/java/mediator/ClientHandler.java
+++ java/src/main/java/mediator/ClientHandler.java
@@ -47,7 +47,6 @@ public class ClientHandler implements Runnable {
                             reply = new Request(request.getType());
                             Order order = request.getOrder();
                             System.out.println(order.getAddress().toString());
-                            //TODO
                             reply.setOrder(databaseManager.getOrderDAOService().create(order.getItems(), order.getAddress(),order.getDateTime(),order.getOrderStatus(),order.getFirstName(), order.getLastName(), order.getEmail()));
                             sendReply(reply);
                             break;
