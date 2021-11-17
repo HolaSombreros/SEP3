ad3bb0dacf21292f36b1949948fc9c2c591cd6b5
stefaniatomuta <304173@via.dk>
2021-11-17 10:59:25 +0100

    enum helper and button disabled in item details :*

--- csharp/SEP3Library/Model/ItemStatus.cs
+++ csharp/SEP3Library/Model/ItemStatus.cs
@@ -1,6 +1,12 @@
-﻿namespace SEP3Library.Model {
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace SEP3Library.Model {
     public enum ItemStatus {
-        OutOfStock,
-        InStock
+        [Description("Out of stock")] OutOfStock,
+        [Description("In stock")] InStock
     }
+
 }
