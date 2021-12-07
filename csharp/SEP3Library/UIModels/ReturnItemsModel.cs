811bcd18f530e8ac93820fda0fb99c1121b51954
realfoxsterdota <foxsterdota@gmail.com>
2021-12-07 10:50:22 +0100

    ReturnItemsModel

---
+++ csharp/SEP3Library/UIModels/ReturnItemsModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using SEP3Library.Models;
+
+namespace SEP3Library.UIModels {
+    public class ReturnItemsModel {
+        public int Id { get; set; }
+        public IList<Item> Items { get; set; }
+    }
+}
